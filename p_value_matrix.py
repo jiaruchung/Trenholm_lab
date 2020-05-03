c5fc75c415b485d2783ad98f812bb7a51d04261c
jiaruchung <jc55248@gmail.com>
2020-05-03 01:06:13 -0400

    added some changes to my repo!

--- p_value_matrix.py
+++ p_value_matrix.py
@@ -110,8 +110,8 @@ def get_urs(filename_list):
             
 # plt.savefig('heatmaps/reverse-c.eps',figsize=(5,5), dpi=600)
 
-condition_1_filenames = glob.glob(os.path.join('./Males-c-excel','*.xlsx'))
-condition_2_filenames = glob.glob(os.path.join('./Males-m-excel','*.xlsx'))
+condition_1_filenames = glob.glob(os.path.join('./Sighted-c-excel','*.xlsx'))
+condition_2_filenames = glob.glob(os.path.join('./Sighted-ddm-excel','*.xlsx'))
 
 urs1 = get_urs(condition_1_filenames)
 urs2 = get_urs(condition_2_filenames)
@@ -146,14 +146,15 @@ result = list(map(lambda row: list(map(lambda element: element.pvalue,
 print('Displaying difference of two samples')
 
 cmap = sns.cm.rocket_r
+sns.set(font_scale=3)
 fig, ax = plt.subplots(1)
 pval_heatmap = sns.heatmap(result, 
                            vmin=0.001, 
                            vmax=0.05, 
                            cmap=cmap,
                            ax=ax,
+                           annot_kws={"size": 14},
                            cbar_kws={'label': 'P-value for the Wilcoxon rank sum statistic'})
-
 cbar = pval_heatmap.collections[0].colorbar
 cbar.set_ticks([0.05, 0.04, 0.03, 0.02, 0.01, 0.001])
 cbar.set_ticklabels(['0.05', '0.04', '0.03', '0.02', '0.01', '0.001'])
