216494a69302ec64708f7f90c3e136a25fdc0d7c
kadjita <k.asumbisa@gmail.com>
2020-05-29 02:04:33 -0400

    change
    now a fxn that returns a fig and bouts data

--- object_bouts.py
+++ object_bouts.py
@@ -5,53 +5,74 @@ Created on Sun May 24 21:43:23 2020
 @author: kasum
 """
 
-from scipy import interpolate
 from scipy.spatial import distance
 import pandas as pd
 import matplotlib.pyplot as plt
 
 
-filename=r'C:\Users\kasum\Downloads\Trenholm_lab-master\pos_object.xlsx'
-
-position_data=pd.read_excel(filename) 
-
-animal_pos=position_data.iloc[:,:2] #Extract animal XY cordinates for all frames
-obj_pos=position_data.iloc[0,2:] #Extracts all the XY fixed object coordinates from frame 1
-
-#Extracting object pos for X and Y. Object pos are arranges in a series of X and Y
-obj_x=[obj_pos[i] for i in range(0,len(obj_pos),2)] #Even i corresponds to X
-obj_y=[obj_pos[i+1] for i in range(0,len(obj_pos),2)] #Odd i corresponds to Y
-
-#Initializing counts for bouts and corresponding animal XY pos for each bout
-obj_bouts=0
-bouts_xpos=[]
-bouts_ypos=[]
-
-#Computing object approach bouts using euclidean distance
-for i in range(len(obj_x)): #iterate through number of object xy pair counts
-    for j in range(len(animal_pos)): 
-        dis=distance.euclidean([obj_x[i],obj_y[i]], [animal_pos.iloc[j,0],animal_pos.iloc[j,1]])
-        if dis< 60: #Threshold for counting bouts
-            obj_bouts+=1
-            bouts_xpos.append(animal_pos.iloc[j,0])
-            bouts_ypos.append(animal_pos.iloc[j,1])
-
-
-######## PLOTS #################################
-#Trajectory plot
-plt.figure()
-path, =plt.plot(animal_pos['X'],animal_pos['Y'],c='grey') # ',' in var defnition allows us to store only 1st item(1d).This makes the handle 1d for easy grp legend calls 
-
-#Object points  plot
-for i in range(0,len(obj_pos),2):
-    obj=plt.scatter(obj_pos[i],obj_pos[i+1],c='black')
-
-#Bouts plot                                       
-bout=plt.scatter(bouts_xpos,bouts_ypos,c='r')
-plt.legend([path,obj,bout],['path','object','bout'],loc='best')
+def object_bouts(filename, bout_distance, framerate):
+    """
+    Args: filename: Path to an excel file with animal pos X and Y in cols 1 and 2 
+        and fixed object XY position in remaining columns i.e X1,Y1,X2,Y2,X3,Y3 
+        corresponing to each object point. ALternatively, excel output from DeepLabcut
+        can simply be used as input. i.e 'C:/Users/kasum/Downloads/Trenholm_lab-master/data.xlsx'
+        
+        bout_distance: an int or float corresponding to the threshold(distance) 
+        for counting bouts. i.e 50
+        framerate: an int or float of the capture frame rate of the camera in Hz. i.e 30 
     
-'''To Do
-Use interpolation to get more points
-#f=interpolate.interp1d(x,y,kind='linear')
+    Returns: 
+        fig: a figure showing trajectory plot, object points and detected bouts
+        bouts: dictionary of bout counts and total duration of bouts in seconds
+        
+    """    
+    position_data=pd.read_excel(filename) #read_excel file with position data
+    
+    animal_pos=position_data.iloc[:,:2] #Extract animal XY cordinates for all frames
+    obj_pos=position_data.iloc[0,2:] #Extracts all the XY fixed object coordinates from frame 1
+    
+    #Extracting object pos for X and Y. Object pos are arranges in a series of X and Y
+    obj_x=[obj_pos[i] for i in range(0,len(obj_pos),2)] #Even i corresponds to X
+    obj_y=[obj_pos[i+1] for i in range(0,len(obj_pos),2)] #Odd i corresponds to Y
+    
+    #Initializing counts for bouts and corresponding animal XY pos for each bout
+    obj_bouts=0
+    bouts_xpos=[]
+    bouts_ypos=[]
+    
+    #Computing object approach bouts using euclidean distance
+    for i in range(len(obj_x)): #iterate through number of object xy pair counts
+        for j in range(len(animal_pos)): 
+            dist=distance.euclidean([obj_x[i],obj_y[i]], [animal_pos.iloc[j,0],animal_pos.iloc[j,1]])
+            if dist <= bout_distance: #Threshold for counting bouts
+                obj_bouts+=1
+                bouts_xpos.append(animal_pos.iloc[j,0])
+                bouts_ypos.append(animal_pos.iloc[j,1])
+    
+    #Dividing the bout count by frame rate gives the totol duration of bouts in secs        
+    bout_duration=obj_bouts/framerate
+        
+    bouts={'tot_bouts':obj_bouts,'bout_duration': bout_duration } #dict of data to be returned
+    
+    ######## PLOTS #################################
+    #Trajectory plot
+    fig=plt.figure()
+    path, =plt.plot(animal_pos.iloc[:,0].values,animal_pos.iloc[:,1].values,c='grey') # ',' in var defnition allows us to store only 1st item(1d).This makes the handle 1d for easy grp legend calls 
+    
+    #Object points plot
+    for i in range(0,len(obj_pos),2):
+        obj=plt.scatter(obj_pos[i],obj_pos[i+1],c='black')
+    
+    #Bouts plot                                       
+    bout=plt.scatter(bouts_xpos,bouts_ypos,c='r', alpha=0.5)
+    plt.legend([path,obj,bout],['path','object','bout'],loc='best')
+    plt.gca().set_yticks([])
+    plt.gca().set_xticks([])
+    plt.show()  
+    
+    return fig, bouts
 
-'''
+
+filename=r'C:\Users\kasum\Downloads\Trenholm_lab-master\data.xlsx' #filepath
+    
+fig,bouts_data=object_bouts(filename,100,30)
