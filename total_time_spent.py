87da5c74d02e56ee7cdb2498866100dcb940e95f
Jia-Ru (Edith) Chung <40964269+jiaruchung@users.noreply.github.com>
2021-01-20 15:46:14 -0500

    Add files via upload

--- total_time_spent.py
+++ total_time_spent.py
@@ -14,8 +14,8 @@ import glob, os
 #####################
 
 #Get all csv files in dir
-file_dir=r'E:\mouse_never_seen_others\day1'
-file=r'E:\mouse_never_seen_others\day1\con1\cleanData\Data_quadrants.csv' #make list of paths
+#file_dir=r'E:\mouse_never_seen_others\day1'
+file=r'F:\Nathan\Ready_for_Python\x_obj0day1\con1\cleanData\Data_quadrants.csv' #make list of paths
 position=pd.read_csv(file)
 
 con1_array=[]
@@ -31,11 +31,11 @@ for i in range(10):
     elif position.object_loc[i]=='bl': 
         con1_array.append(position.bl_dur.values[i])  
 
-return con1_array            
+#return con1_array            
 
 
-os.chdir(r'E:\mouse_never_seen_others\day1\con2')
-file2=r'E:\mouse_never_seen_others\day1\con2\cleanData\Data_quadrants.csv' #make list of paths
+#os.chdir(r'E:\mouse_never_seen_others\day1\con2')
+file2=r'F:\Nathan\Ready_for_Python\x_obj0day1\con2\cleanData\Data_quadrants.csv' #make list of paths
 position2=pd.read_csv(file2)
 
 con2_array=[]
@@ -51,15 +51,15 @@ for i in range(10):
     elif position2.object_loc[i]=='bl': 
         con2_array.append(position2.bl_dur.values[i])  
 
-return con2_array
+#return con2_array
 
 
-    first = pd.DataFrame(con1_array, columns =['con1']) 
-    second = pd.DataFrame(con2_array, columns =['con2'])
-    result = pd.concat([first, second], axis=1, join="inner")
+first = pd.DataFrame(con1_array, columns =['con1'])
+second = pd.DataFrame(con2_array, columns =['con2'])
+result = pd.concat([first, second], axis=1, join="inner")
 
-    #Saving new csv to file_dir
-    new_dir=r'E:\Nathan\Results'
-    file_name = "total_time_spent_mnsom"
-    result.to_excel(new_dir+'/'+file_name+'.xlsx', index=False)
+#Saving new csv to file_dir
+new_dir=r'F:\Nathan\Results'
+file_name = "total_time_spent_obj1"
+result.to_excel(new_dir+'/'+file_name+'.xlsx', index=False)
 
